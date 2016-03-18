7a3e6b8e50e5e2053ce0cfd40f7c1c359ce31011
Erik Norgren <enorgren90@gmail.com>
2016-03-18 12:09:00 +0100

    Fixed gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,38 @@
-var gulp   = require('gulp');
-var shell  = require('gulp-shell');
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+var tsc   = require('gulp-tsc');
+
+var paths = {
+    tscripts : {
+        src : [
+            'src/**/*.ts',
+            'typings/main.d.ts'
+        ],
+        dest : 'dist'
+    }
+};
 
 gulp.task('default', ['run']);
 
-gulp.task('run', shell.task([
-  'ts-node src/example.ts'
+gulp.task('ts:run', shell.task([
+    'ts-node src/example.ts'
 ]));
+
+gulp.task('run', ['compile:typescript'], shell.task([
+    'node dist/example.js'
+]));
+
+gulp.task('watch', function () {
+  gulp.watch(paths.tscripts.src, ['compile:typescript']);
+});
+
+gulp.task('compile:typescript', function () {
+  return gulp
+      .src(paths.tscripts.src)
+      .pipe(tsc({
+        emitError: false,
+        target: 'ES5',
+        sourceMap:true
+      }))
+      .pipe(gulp.dest(paths.tscripts.dest));
+});
